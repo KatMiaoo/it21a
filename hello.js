f3522852fa8f73171572d288c82ffa37ce849cfa
Kimberly <kaisermeatriderr@gmail.com>
2025-08-28 10:55:57 +0800

    addition, subtraction, multiplication, division

--- hello.js
+++ hello.js
@@ -6,4 +6,9 @@ num2 = 3;
 sum = num1 + num2;
 diff = num1 - num2;
 prod = num1 * num2;
-quo = num1 / num2;
+qou = num1 / num2;
+
+console.log("The sum of " + num1 + " & " + num2 + " is " + sum);
+console.log("The differnece of " + num1 + " & " + num2 + " is " + diff);
+console.log("The product of " + num1 + " & " + num2 + " is " + prod);
+console.log("The quotient of " + num1 + " & " + num2 + " is " + qou);

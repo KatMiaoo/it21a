8b2448e71c130dc3e375bb180f512868a7488530
JOHNANDERSON7 <johnanderson312006@gmail.com>
2025-09-18 08:44:34 +0800

    Added lab/hero

---
+++ hero.js
@@ -0,0 +1,34 @@
+class Hero{
+    #health;
+    constructor (name,health,attack){
+        this.name = name;
+        this.health = health;
+        this.attack = attack;
+    }
+
+    getHealth(){
+        return this.#health;
+    }
+}
+
+class Warrior extends Hero{
+    useAbility(){
+        console.log(`${this.name} uses power strike`);
+    }
+}
+
+class Mage extends Hero {
+    constructor(name,health,attack,mana){
+        super(name,health,attack);
+        this.mana = mana;
+    }
+    useAbility(){
+        console.log(`${this.name} cast expelliarmus`);
+    }
+
+}
+const warrior = new Warrior("Kit",100,10);
+warrior.useAbility();
+
+const mage = new Mage("Kat",90,8,50);
+mage.useAbility();
